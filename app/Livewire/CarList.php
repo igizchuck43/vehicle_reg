bfb3a00e5dd66869be0199fb82d0bd0df4a488d7
Igiz <igizstarkle@gmail.com>
2024-09-07 14:11:04 +0300

    Car Blade file in Livewire

---
+++ app/Livewire/CarList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CarList extends Component
+{
+    public function render()
+    {
+        return view('livewire.car-list');
+    }
+}
---
+++ resources/views/livewire/car-list.blade.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            Vehicle Registration
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+</div>
